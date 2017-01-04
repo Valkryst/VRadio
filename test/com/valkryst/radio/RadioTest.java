1b54ee87f8ae5afa70914d745f3e00ca604f3d07
Valkryst <valkryst@valkryst.com>
2017-01-04 19:43:33 -0400

    Slight test updated. Moved tests around to differnet files.

--- test/com/valkryst/radio/RadioTest.java
+++ test/com/valkryst/radio/RadioTest.java
@@ -1,60 +1,15 @@
 package com.valkryst.radio;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class RadioTest {
     @Test
-    public void transmitA() {
-        final Radio<String> radio = new Radio<>();
-        final ReceiverTest receiver = new ReceiverTest();
-
-        radio.addReceiver("Test Event", receiver);
-        radio.transmit("Test Event");
-
-        Assert.assertEquals(receiver.getData(), null);
-    }
-
-    @Test
-    public void transmitB() {
-        final Radio<String> radio = new Radio<>();
-        final ReceiverTest receiver = new ReceiverTest();
-
-        radio.addReceiver("Test Event", receiver);
-        radio.transmit("Test Event", null);
-
-        Assert.assertEquals(receiver.getData(), null);
-    }
-
-    @Test
-    public void transmitC() {
-        final Radio<String> radio = new Radio<>();
-        final ReceiverTest receiver = new ReceiverTest();
-
-        radio.addReceiver("Test Event", receiver);
-        radio.transmit("Test Event", "Hello");
-
-        Assert.assertEquals(receiver.getData(), "Hello");
-    }
-
-    @Test
-    public void transmitD() {
-        final Radio<String> radio = new Radio<>();
-        final ReceiverTest receiver = new ReceiverTest();
-
-        radio.addReceiver("Test Event", receiver);
-        radio.transmit("Other Event", "Hello");
-
-        Assert.assertEquals(receiver.getData(), "");
-    }
-
-    @Test(expected=NullPointerException.class)
     public void removeReceiverA() {
         final Radio<String> radio = new Radio<>();
         radio.removeReceiver(null, null);
     }
 
-    @Test(expected=NullPointerException.class)
+    @Test
     public void removeReceiverB() {
         final Radio<String> radio = new Radio<>();
         radio.removeReceiver("Test Event", null);
@@ -77,7 +32,7 @@ public class RadioTest {
         radio.removeReceiver("Test Event", receiver);
     }
 
-    @Test(expected=NullPointerException.class)
+    @Test
     public void removeReceiversA() {
         final Radio<String> radio = new Radio<>();
         radio.removeReceivers(null);
--- test/com/valkryst/radio/ReceiverTest.java
+++ test/com/valkryst/radio/ReceiverTest.java
@@ -1,5 +1,8 @@
 package com.valkryst.radio;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ReceiverTest implements Receiver<String> {
     private String data = "";
 
@@ -10,5 +13,49 @@ public class ReceiverTest implements Receiver<String> {
 
     public String getData() {
         return data;
+    }
+
+    @Test
+    public void transmitA() {
+        final Radio<String> radio = new Radio<>();
+        final ReceiverTest receiver = new ReceiverTest();
+
+        radio.addReceiver("Test Event", receiver);
+        radio.transmit("Test Event");
+
+        Assert.assertEquals(receiver.getData(), null);
+    }
+
+    @Test
+    public void transmitB() {
+        final Radio<String> radio = new Radio<>();
+        final ReceiverTest receiver = new ReceiverTest();
+
+        radio.addReceiver("Test Event", receiver);
+        radio.transmit("Test Event", null);
+
+        Assert.assertEquals(receiver.getData(), null);
+    }
+
+    @Test
+    public void transmitC() {
+        final Radio<String> radio = new Radio<>();
+        final ReceiverTest receiver = new ReceiverTest();
+
+        radio.addReceiver("Test Event", receiver);
+        radio.transmit("Test Event", "Hello");
+
+        Assert.assertEquals(receiver.getData(), "Hello");
+    }
+
+    @Test
+    public void transmitD() {
+        final Radio<String> radio = new Radio<>();
+        final ReceiverTest receiver = new ReceiverTest();
+
+        radio.addReceiver("Test Event", receiver);
+        radio.transmit("Other Event", "Hello");
+
+        Assert.assertEquals(receiver.getData(), "");
     }
 }
